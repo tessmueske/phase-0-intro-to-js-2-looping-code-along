824ecddb50fd0e04713d0334018f51542be7bc74
Tess <tess.mueske@gmail.com>
2023-11-12 14:49:43 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+
+function writeCards(names, gift) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    let message = "Thank you, " + names[i] + ", for the wonderful surprise gift!";
+    messages.push(message);
+  }
+
+  return messages;
+}
