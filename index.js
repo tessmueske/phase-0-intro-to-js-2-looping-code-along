7b22bcbb6644d167b61cc8d5b4f17ff350305191
Tess <tess.mueske@gmail.com>
2024-01-20 12:17:01 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
-function countDown(number) {
-    for (let i = number; i >= 0; i--) {
-      console.log(i);
-    }
+for (let age = 30; age < 40; age++) {
+  console.log (`I'm ${age} years old. Happy birthday to me!`);
+}
+
+const gifts = ["teddy bear","drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
   }
+  return gifts;
+}
 
-function writeCards(names, gift) {
+wrapGifts(gifts);
+
+function writeCards(names) {
   let messages = [];
-
-  for (let i = 0; i < names.length; i++) {
-    let message = "Thank you, " + names[i] + ", for the wonderful surprise gift!";
+  for (let name of names){
+    let message = `Thank you, ${name}, for the wonderful surprise gift!`
     messages.push(message);
   }
-
   return messages;
+}
+
+let names = ["Charlie", "Samip", "Ali"];
+let result = writeCards(names);
+console.log(result);
+
+function countDown(){
+  let count = 10
+  while (count >= 0) {
+    console.log(count --)
+  }
 }
